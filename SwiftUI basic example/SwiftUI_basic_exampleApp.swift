2bed9fa5648f476ad4eedd904ef1ea2f874717c8
rizkym <rizkymashudi7@gmail.com>
2022-05-05 08:48:41 +0700

    Initial Commit

---
+++ SwiftUI basic example/SwiftUI_basic_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_basic_exampleApp.swift
+//  SwiftUI basic example
+//
+//  Created by Rizky Mashudi on 05/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_basic_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
